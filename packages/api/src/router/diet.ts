180a2080f81563f9abcd568a4d03f91652ed6851
Juan Lopez <juanpablolopez43@gmail.com>
2023-08-03 12:14:53 -0300

    fix types

--- packages/api/src/router/diet.ts
+++ packages/api/src/router/diet.ts
@@ -1,15 +1,14 @@
 import { createDietHandler } from '../controllers/diet.controller';
 import { createDietSchema } from '../schema/diet.schema';
-import { createTRPCRouter, protectedProcedure, publicProcedure } from '../trpc';
+import { createTRPCRouter, publicProcedure } from '../trpc';
 
 export const dietRouter = createTRPCRouter({
   createDiet: publicProcedure.input(createDietSchema).mutation(async ({ ctx, input }) => {
-    console.log('estoy en el router')
     const diet = await createDietHandler({ctx, input});
     return diet
   }),
 
-  getDiets : publicProcedure.query(({ ctx, input }) => {
+  getDiets : publicProcedure.query(({ ctx}) => {
     return ctx.prisma.diet.findMany()
   })
 });
